8f845a973b2dba96611ecab32237ab2771e66208
Michel Schmid <michel.schmid@robotics-erlangen.de>
2024-02-16 19:18:20 +0100

    add <vcpkg path> as prefix for all packages

--- cmake/modules/FindOrBuildProtobuf.cmake
+++ cmake/modules/FindOrBuildProtobuf.cmake
@@ -1,6 +1,7 @@
 # sanitize environment before find_package, because otherwise it also looks in the directory created for the ExternalProject
 include(EnvHelper)
 sanitize_env()
+set(CMAKE_PREFIX_PATH "<vcpkg path>/vcpkg_installed/x64-windows/share/protobuf")
 find_package(Protobuf 3.3.0)
 restore_env()
 
